f2bd98642904b17a9f814f23c0cbf1e1a04d68be
Пользователь <mkomp06@gmail.com>
2023-11-10 03:06:57 +0300

    methods for tasks

---
+++ main/main/calculator.h
@@ -0,0 +1,30 @@
+#pragma once
+#define max_number 20 
+class Calculator {
+private:
+	double x, y,b;
+public:
+	Calculator() = default;
+	Calculator(double x, double y,double b) {
+		if (x >= -max_number && x <= max_number) this->x = x;
+		if (y >= -max_number && y <= max_number) this->y = y;
+		if (b >= -max_number && b <= max_number) this->b = b;
+	}
+	double getx()const;
+	double gety() const;
+	double getb() const;
+	double perimeter(Calculator obj, double coeficient) const;
+	double square(Calculator obj) const;
+	double circumference(Calculator obj) const;
+	double volume(Calculator obj, double coeficient) const;
+	double surface_area(Calculator obj) const;
+	double square_circle(Calculator obj) const;
+	double average(Calculator obj)const;
+	double geometric_mean(Calculator obj) const;
+	void da_xyu_snaet(Calculator obj) const;
+	void da_xyu_snaet_v2(Calculator obj) const;
+	double hypotenuse(Calculator obj) const;
+	double distance_between_points(Calculator obj) const;
+	double herons_formula(Calculator obj)const;
+	void swapp(Calculator& obj)const;
+};
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,23 +1,69 @@
 #include <iostream> 
-#define max_number 20 
-class Calculator {
-private:
-	double x, y;
-public:
-	Calculator() = default;
-	Calculator(double x, double y) {
-		if (x >= -max_number && x <= max_number) this->x = x;
-		if (y >= -max_number && y <= max_number) this->y = y;
-	}
-	double getx();
-	double gety();
-
-};
-double Calculator::getx() { return x; }
-double Calculator::gety() { return y; }
+#include "calculator.h"
+#include <algorithm>
+#define PI 3.1415
+double Calculator::getx() const { return x; }
+double Calculator::gety() const { return y; }
+double Calculator::getb() const { return b; }
+double Calculator::perimeter(Calculator obj, double coeficient) const{//1,3 12
+	return coeficient*(obj.getx()+obj.gety() + obj.getb());
+}
+double Calculator::square(Calculator obj) const{//2,3,5
+	return obj.gety() * obj.getx() * obj.getb();
+}
+double Calculator::circumference(Calculator obj)const {//4,7,14 
+	return PI * obj.getx();
+}
+double Calculator::volume(Calculator obj, double coeficient) const {//5,6
+	return coeficient * square(obj);
+}
+double Calculator::surface_area(Calculator obj) const {//6
+	return 2 * ((obj.getx() * obj.getb()) + (obj.getb() + obj.gety()) + (obj.getx() * obj.gety()));
+}
+double Calculator::square_circle(Calculator obj) const {//7 ,13 ,14 ,15
+	return PI * square(obj);
+}
+//double Calculator::average(Calculator obj,double sum) const {// 8 
+//	int count = this -> getx();///quet
+//	for (; count != obj.gety(); count++)
+//	{
+//		sum += count;///quet 
+//	}
+//	return sum / obj.gety();
+//}
+double Calculator::average(Calculator obj) const {// 8
+	return (obj.getx() + obj.gety()) / 2;
+}
+double Calculator::geometric_mean(Calculator obj) const {// 9
+	return sqrt(obj.getx() * obj.gety());
+}
+void Calculator::da_xyu_snaet(Calculator obj) const  {//10
+	std::cout << obj.getx() * obj.getx() + obj.gety() * obj.gety() <<"\n";
+	std::cout << obj.getx() * obj.getx() - obj.gety() * obj.gety() <<"\n";
+	std::cout << obj.getx() * obj.getx() * obj.gety() * obj.gety() <<"\n";
+	std::cout << obj.getx() * obj.getx() / obj.gety() * obj.gety() <<"\n";
+}
+void Calculator::da_xyu_snaet_v2(Calculator obj) const {//11
+	std::cout << std::fabs(obj.getx()) + std::fabs(obj.gety()) << "\n";
+	std::cout << std::fabs(obj.getx()) - std::fabs(obj.gety()) << "\n";
+	std::cout << std::fabs(obj.getx()) * std::fabs(obj.gety()) << "\n";
+	std::cout << std::fabs(obj.getx()) / std::fabs(obj.gety()) << "\n";
+}
+double Calculator::hypotenuse(Calculator obj) const {//12,20
+	return std::sqrt(std::pow(obj.getx(), 2) + std::pow(obj.gety(), 2));
+}
+double Calculator::distance_between_points(Calculator obj) const{//16 ,17,18,19,20
+	return std::fabs(obj.getx() - obj.gety());
+}
+double Calculator::herons_formula(Calculator obj)const  {//21
+	return std::sqrt(perimeter(obj, 1. / 2) * (perimeter(obj, 1. / 2) - obj.getx()) * (perimeter(obj, 1. / 2) - obj.gety()) * (perimeter(obj, 1. / 2) - obj.getb()));
+}
+void Calculator::swapp(Calculator& obj)const {///quels
+	std::swap(x, obj.x);
+	std::swap(y, obj.y);
+}
 int main()
 {
-	Calculator obj(2, 2);
-	std::cout << obj.getx() << " ";
-	std::cout << obj.gety();
+	Calculator obj(2,2,2);
+	std::cout<<obj.herons_formula(obj);
 }
